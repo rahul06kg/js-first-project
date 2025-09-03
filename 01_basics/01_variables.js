be2d88d211816adecf0380062e00e626be2b7af2
rahul06kg <40162117+rahul06kg@users.noreply.github.com>
2025-09-03 11:53:22 +0000

    new files created

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+
+const accountId = 1223344
+let accountEmail = "rahul@google.com"
+var accountPassword = "123456"
+accountCity = "Kolkata";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
