2c41ae6d2eb699d3b3889b7ab1469669dce8fd74
rahul06kg <40162117+rahul06kg@users.noreply.github.com>
2025-09-03 12:14:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// dataTypes
+
+// premitive dataTypes
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value / empty value
+// undefined => when we don't declare the value
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // output is always undefined
+console.log(typeof null); // output object
+
