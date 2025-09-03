c24f77608d1fedb880d205f1af35258e0810f944
rahul06kg <40162117+rahul06kg@users.noreply.github.com>
2025-09-03 14:40:02 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log(2 < 1); // comparision check
+// console.log(2 >= 1);
+// console.log(2 == 1); // equality check
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// === strict comparision
+
+console.log("2" === 2);
