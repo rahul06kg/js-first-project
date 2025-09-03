c44907cddcfe8cca8f5ad6e4166cf7a37f8d6cf2
rahul06kg <40162117+rahul06kg@users.noreply.github.com>
2025-09-03 14:24:23 +0000

    operations with operators ++

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,3 +17,30 @@ console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false
 // "Rahul" => true
+
+
+
+
+/* ********************** operations ********************** */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Rahul"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+//president operator (++)
+let gameCounter = 100;
+console.log(++gameCounter); // prefix
+console.log(gameCounter++); // postfix
+
+
+// till video 7
