2b2fe35a84fd90ba6483ce82cdc8e7d7e1d91d48
rahul06kg <40162117+rahul06kg@users.noreply.github.com>
2025-09-04 12:03:06 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "Rahul";
+const repoCount = 50;
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`) //backtick
+
+const gameName = new String('Rahul Gupta');
+
+// console.log(gameName);
+
+const newStringOne = "    Rahul    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://rahul.google.com/in/my%20profile"
+// console.log(url.replace('%20', '-'));
+
+console.log(gameName.split("").reverse().join(""));
+
+
