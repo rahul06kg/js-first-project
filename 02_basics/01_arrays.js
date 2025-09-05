9b5bf9d89c0ec4a244e5da0460e90997dd5318da
rahul06kg <40162117+rahul06kg@users.noreply.github.com>
2025-09-05 08:38:59 +0000

    02 basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const newArr = ["Shaktiman", "Nagraj"]
+const myArr2 = new Array(1, 2, 3, 4)
+
+//console.log(myArr[0]);
+
+// Array methods
+myArr.push(6) //push is to add at last
+myArr.push(7)
+myArr.pop() //delete/remove from last no argument needed
+myArr.unshift(9) //to add in first place
+myArr.shift() //to add in first place no argument needed
+
+console.log(myArr);
